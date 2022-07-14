a74d78420ffb68aee82264f72714fa0133ad070a
Prasad4488 <tepugadeprasad@gmail.com>
2022-07-14 18:34:30 +0530

    die problem commit

---
+++ die.sh
@@ -0,0 +1,32 @@
+declare -A die
+ran=$((1+$RANDOM%6))
+	die[0]=$ran
+	echo ${die[0]}
+ran=$((1+$RANDOM%6))
+        die[1]=$ran
+        echo ${die[1]}
+ran=$((1+$RANDOM%6))
+        die[2]=$ran
+        echo ${die[2]}
+ran=$((1+$RANDOM%6))
+        die[3]=$ran
+        echo ${die[3]}
+ran=$((1+$RANDOM%6))
+        die[4]=$ran
+        echo ${die[4]}
+ran=$((1+$RANDOM%6))
+        die[5]=$ran
+        echo ${die[5]}
+ran=$((1+$RANDOM%6))
+        die[6]=$ran
+        echo ${die[6]}
+ran=$((1+$RANDOM%6))
+        die[7]=$ran
+        echo ${die[7]}
+ran=$((1+$RANDOM%6))
+        die[8]=$ran
+        echo ${die[8]}
+ran=$((1+$RANDOM%6))
+        die[9]=$ran
+        echo ${die[9]}
+
