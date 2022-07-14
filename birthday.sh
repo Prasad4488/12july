06e1094feb7bdf97447a10cf86220237d646c586
Prasad4488 <tepugadeprasad@gmail.com>
2022-07-14 20:56:37 +0530

    edited

--- birthday.sh
+++ birthday.sh
@@ -1,9 +1,17 @@
-read -p "enter month " x
-read -p "enter year " y
-a="birthday on $x $y"
-echo $a
-for (( i=1;i<=50;i++ ))
-do
-	echo $a
+read bdy
+bmon=${bdy:0:2}
+bday=${bdy:3:2}
+byear=${bdy:6:4}
 
-done
+cdate='date +%m-%d-%y'
+cmon=${cdate:0:2}
+cday=${cdate:3:2}
+cyear=${cdate:6:4}
+
+if [[ "$cmon" -lt "$bmon" ]] || [[ "$cmon" -eq "$bmon" && "$cday" -eq "$bday" ]]
+then
+	let age=cyear-byear-1
+else
+	let age=cyear-byear
+fi
+	echo "age= "$age
